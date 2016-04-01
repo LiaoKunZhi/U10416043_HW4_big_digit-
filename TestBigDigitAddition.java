9828ac56804f28e5e5571e67b18543d5bf0ba12b
LiaoKunZhi <a0956823675@gmail.com>
2016-04-01 20:38:00 +0800

    Create TestBigDigitAddition.java-1
    Create a new class to start the program, and make a scanner inside the main method.

---
+++ TestBigDigitAddition.java
@@ -0,0 +1,15 @@
+//U10416043   廖崑智
+
+import java.util.Scanner;
+
+//Class
+public class TestBigDigitAddition{
+
+	//Main method
+	public static void main(String[] args){
+
+		//Make a scanner
+		Scanner input = new Scanner(System.in);
+		
+			}
+}
