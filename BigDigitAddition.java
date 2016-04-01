e85b8c67ba87e0137ef2466cd2ea2ec4aa3833b8
LiaoKunZhi <a0956823675@gmail.com>
2016-04-01 21:12:36 +0800

    Create BigDigitAddition.java-1
    Create a new class to addition.First,make three new array lists to carry the big numbers.

---
+++ BigDigitAddition.java
@@ -0,0 +1,15 @@
+//U10416043   廖崑智
+
+import java.util.Scanner;
+import java.lang.*;
+import java.util.*;
+
+//Class
+public class BigDigitAddition{
+
+	//Make new ArrayLists
+	ArrayList<String> listA = new ArrayList<String>();
+	ArrayList<String> listB = new ArrayList<String>();
+	ArrayList<String> addition = new ArrayList<String>();
+	
+}
